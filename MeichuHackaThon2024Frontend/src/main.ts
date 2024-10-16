6e72a58db663f124436bd7bfe069039fb741a004
Jacky <jackywu526@gmail.com>
2024-10-16 10:48:57 +0800

    feat(main.ts): 新增 icon lib

--- MeichuHackaThon2024Frontend/src/main.ts
+++ MeichuHackaThon2024Frontend/src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core';
 
@@ -15,11 +14,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 /* import specific icons */
 import {
-  faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faCaretDown, faBars, faUpload, faCopy, faMagnifyingGlass
+  faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faCaretDown, faBars, faUpload, faCopy, faMagnifyingGlass, faShoePrints, faBicycle, faBus, faForwardStep, faBackwardStep
 } from '@fortawesome/free-solid-svg-icons';
 
 /* add icons to the library */
-library.add(faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faCaretDown, faBars, faUpload, faCopy, faMagnifyingGlass);
+library.add(faArrowsDownToLine, faArrowsTurnRight, faUserPlus, faCaretDown, faBars, faUpload, faCopy, faMagnifyingGlass, faShoePrints, faBicycle, faBus, faForwardStep, faBackwardStep);
 
 const app = createApp(App)
 
