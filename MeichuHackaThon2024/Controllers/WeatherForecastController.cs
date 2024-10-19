e69083424c1456cf02fc0a4174dd863ef4fabee0
DustinYang <asd9532100@gmail.com>
2024-10-19 22:32:38 +0800

    移除不必要的方法

--- MeichuHackaThon2024/Controllers/WeatherForecastController.cs
+++ MeichuHackaThon2024/Controllers/WeatherForecastController.cs
@@ -6,50 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MeichuHackaThon2024.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
-    [Route("DataBase/[controller]")]
-    [ApiController]
-    public class DataBaseController : ControllerBase
-    {
-        private readonly MeichuHackaThonDBContext _context_info;
-        public DataBaseController(MeichuHackaThonDBContext context_info)
-        {
-            _context_info = context_info;
-        }
-
-        //[HttpGet(Name = "DataBase")]
-        //public IEnumerable<MeichuHackaThon2024Model.Models.PathDetail> Get()
-        //{
-        //    return _context_info.Paths;
-        //}
-    }
     [Route("[controller]/[action]")]
     [ApiController]
     public class PathController : ControllerBase
@@ -79,7 +35,7 @@ namespace MeichuHackaThon2024.Controllers
         public PathDetailViewModel getPathList(int Sysno)
         {
             PathDetailViewModel tesxt = new PathDetailViewModel();
-            tesxt = _IPathInfoData.Path(Sysno);
+            tesxt = _IPathInfoData.Path(1);
             return tesxt;
         }
     }
